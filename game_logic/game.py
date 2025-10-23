5a046a4f609b94aba5620b35c64f7fe0511bcf98
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-23 09:38:03 +0300

    add a files

---
+++ game_logic/game.py
@@ -0,0 +1,3 @@
+def create_player(name:str) -> dict
+def init_game()->dict
+def play_round(p1:dict,p2:dict)
---
+++ utils/deck.py
@@ -0,0 +1,8 @@
+def create_card(rank:str,suite:str) -> dict:
+    cards = {2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,"J":11,"Q":12,"K":13,"A":14}
+    
+def compare_cards(p1_card:dict, p2_card:dict) -> str:
+def create_deck() -> list[dict]:
+    return
+def shuffle(deck:list[dict]) -> list[dict]:
+    pass
