a2e92a53a89c80f610ac068d57f394ae4c8ef9f6
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-23 23:50:39 +0300

    Update in game file

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,7 +1,18 @@
-def create_player(name:str) -> dict:
-    
-    return {}
+from utils.deck import create_deck,shuffle
+
+def create_player(name:str = "AI") -> dict:
+    player = {"name":name,"hand":[],"won_pil":[]}
+    return player
+
 def init_game()->dict:
-    return {}
+    game_dict = {}
+    player_1 = create_player("yy")
+    player_2 = create_player()
+
+    shuffle_ = shuffle(create_deck())
+    p1 = shuffle_[:26]
+    p2 = shuffle_[26:]
+
+    return game_dict
 def play_round(p1:dict,p2:dict):
-    
+    pass
